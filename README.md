869277b14e5aa71cb565b5557c0e8167607de6e9
kazuki-onodera <kazuki.onodera@dena.com>
2019-08-16 19:11:39 +0900

    update train.py

--- README.md
+++ README.md
@@ -6,8 +6,13 @@
 * DATALAKE_TEST_ID: DataLake item id
 
 ### Features
-* USECOLS: Names of features. e.g. var_1,var_2,var_3
-* TARGET: Name of target column. This Environment key is required.
+* INPUT_FIELDS: Names of features. e.g. var_1,var_2,var_3
+* TARGET_FIELD: Name of target column. This Environment key is required.
 
 ### LightGBM
-* PARAMS: Parameters to be passed to lgb.train as params. e.g. num_boost_round=200,learning_rate=0.2
+* PARAMS: Parameters to be passed to lgb.cv as params. e.g. num_boost_round=200,learning_rate=0.2
+Refet to https://lightgbm.readthedocs.io/en/latest/Parameters.html
+* NFOLD: to be passed to nfold
+* EARLY_STOPPING_ROUNDS: to be passed to early_stopping_rounds
+* VERBOSE_EVAL: to be passed to verbose_eval
+* STRATIFIED: to be passed to stratified
--- train.py
+++ train.py
@@ -8,6 +8,7 @@ from pathlib import Path
 from math import modf
 
 import pandas as pd
+import numpy as np
 import lightgbm as lgb
 
 from abeja.datalake import Client as DatalakeClient
@@ -16,16 +17,17 @@ from abeja.datalake import Client as DatalakeClient
 
 # Configurable, but you don't need to set this. Default is "~/.abeja/.cache" in ABEJA Platform.
 ABEJA_STORAGE_DIR_PATH = os.getenv('ABEJA_STORAGE_DIR_PATH')
+ABEJA_TRAINING_RESULT_DIR = os.getenv('ABEJA_TRAINING_RESULT_DIR')
 
 DATALAKE_CHANNEL_ID = os.getenv('DATALAKE_CHANNEL_ID')
 DATALAKE_TRAIN_ID = os.getenv('DATALAKE_TRAIN_ID')
 DATALAKE_TEST_ID = os.getenv('DATALAKE_TEST_ID')
 
 # params for load
-USECOLS = os.getenv('USECOLS')
-TARGET = os.getenv('TARGET') # required
+INPUT_FIELDS = os.getenv('INPUT_FIELDS')
+TARGET_FIELD = os.getenv('TARGET_FIELD') # required
 
-if TARGET is None:
+if TARGET_FIELD is None:
     raise Exception(f'TARGET is required')
 
 # params for lgb
@@ -49,13 +51,78 @@ for kv in PARAMS.split(','):
     except:
         params[k] = v
 
+NFOLD = os.getenv('NFOLD')
+if NFOLD is None:
+    NFOLD = 5
 
+EARLY_STOPPING_ROUNDS = os.getenv('EARLY_STOPPING_ROUNDS')
+if EARLY_STOPPING_ROUNDS is not None:
+    EARLY_STOPPING_ROUNDS = int(EARLY_STOPPING_ROUNDS)
 
+VERBOSE_EVAL = os.getenv('VERBOSE_EVAL')
+if VERBOSE_EVAL is not None:
+    VERBOSE_EVAL = int(VERBOSE_EVAL)
+
+STRATIFIED  = os.getenv('STRATIFIED')
+if STRATIFIED is not None:
+    STRATIFIED = bool(STRATIFIED)
+else:
+    STRATIFIED = True
 
 # =============================================================================
 # 
 # =============================================================================
+class ModelExtractionCallback(object):
+    """
+    original author : momijiame
+    ref : https://blog.amedama.jp/entry/lightgbm-cv-model
+    description : Class for callback to extract trained models from lightgbm.cv(). 
+    note: This class depends on private class '_CVBooster', so there are some future risks. 
+    
+    usage:
+        extraction_cb = ModelExtractionCallback()
+        callbacks = [extraction_cb,]
+    
+        lgb.cv(params, dtrain, nfold=5, 
+               num_boost_round=9999,
+               early_stopping_rounds=EARLY_STOPPING_ROUNDS,
+               verbose_eval=verbose_eval,
+               callbacks=callbacks,
+               seed=0)
+        
+        models = extraction_cb.raw_boosters
+    
+    """
 
+    def __init__(self):
+        self._model = None
+
+    def __call__(self, env):
+        # _CVBooster の参照を保持する
+        self._model = env.model
+
+    def _assert_called_cb(self):
+        if self._model is None:
+            # コールバックが呼ばれていないときは例外にする
+            raise RuntimeError('callback has not called yet')
+
+    @property
+    def boosters_proxy(self):
+        self._assert_called_cb()
+        # Booster へのプロキシオブジェクトを返す
+        return self._model
+
+    @property
+    def raw_boosters(self):
+        self._assert_called_cb()
+        # Booster のリストを返す
+        return self._model.boosters
+
+    @property
+    def best_iteration(self):
+        self._assert_called_cb()
+        # Early stop したときの boosting round を返す
+        return self._model.best_iteration
 
 def handler(context):
     print('Start train handler.')
@@ -67,14 +134,14 @@ def handler(context):
     datalake_file.get_content(cache=True)
     
     csvfile = Path(ABEJA_STORAGE_DIR_PATH, DATALAKE_CHANNEL_ID, DATALAKE_TRAIN_ID)
-    if USECOLS is None:
+    if INPUT_FIELDS is None:
         train = pd.read_csv(csvfile)
     else:
-        usecols = USECOLS.split(',')
-        train = pd.read_csv(csvfile, usecols=usecols+[TARGET])
+        usecols = INPUT_FIELDS.split(',')
+        train = pd.read_csv(csvfile, usecols=usecols+[TARGET_FIELD])
     
-    y_train = train[TARGET].values
-    cols_drop = [c for c in train.columns if train[c].dtype == 'O'] + [TARGET]
+    y_train = train[TARGET_FIELD].values
+    cols_drop = [c for c in train.columns if train[c].dtype == 'O'] + [TARGET_FIELD]
     train.drop(cols_drop, axis=1, inplace=True)
     X_train = train
     cols_train = X_train.columns.tolist()
@@ -82,13 +149,20 @@ def handler(context):
     
     dtrain = lgb.Dataset(X_train, y_train)
     
-    model = lgb.train(params, train_set=dtrain, #num_boost_round=num_boost_round, 
-                      valid_sets=dtrain, valid_names=None, fobj=None, feval=None, 
-                      init_model=None, feature_name='auto', categorical_feature='auto', 
-                      early_stopping_rounds=None, evals_result=None, 
-                      verbose_eval=True, learning_rates=None, 
-                      keep_training_booster=False, callbacks=None)
+    extraction_cb = ModelExtractionCallback()
+    callbacks = [extraction_cb,]
     
+    lgb.cv(params, dtrain, nfold=NFOLD,
+           early_stopping_rounds=EARLY_STOPPING_ROUNDS,
+           verbose_eval=VERBOSE_EVAL,
+           stratified=STRATIFIED,
+           callbacks=callbacks,
+           seed=0)
+    
+    models = extraction_cb.raw_boosters
+    for i,model in enumerate(models):
+        model.save_model(os.path.join(ABEJA_TRAINING_RESULT_DIR, f'model_{i}.txt'))
+        
     del dtrain, X_train; gc.collect()
     
     # load test
@@ -100,7 +174,11 @@ def handler(context):
         
         csvfile = Path(ABEJA_STORAGE_DIR_PATH, DATALAKE_CHANNEL_ID, DATALAKE_TEST_ID)
         X_test = pd.read_csv(csvfile, usecols=cols_train)[cols_train]
-        pred = model.predict(X_test)
+        
+        pred = np.zeros(len(X_test))
+        for model in models:
+            pred += model.predict(X_test)
+        pred /= len(models)
         
         print(pred)
     
